63e308858edc1535b627dbc447700bc4e8b76a58
Ghaida-Aloufi <ghaida2219@gmail.com>
2024-09-12 13:36:33 +0300

    add lab

--- main.js
+++ main.js
@@ -1,54 +1,54 @@
-let button1=document.getElementById("one")
-let button2=document.getElementById("two")
-let button3=document.getElementById("three")
-let button4=document.getElementById("four")
-let button5=document.getElementById("five")
-let button6=document.getElementById("six")
-let button7=document.getElementById("seven")
-let button8=document.getElementById("eight")
-let button9=document.getElementById("nine")
+let button1 = document.getElementById("one")
+let button2 = document.getElementById("two")
+let button3 = document.getElementById("three")
+let button4 = document.getElementById("four")
+let button5 = document.getElementById("five")
+let button6 = document.getElementById("six")
+let button7 = document.getElementById("seven")
+let button8 = document.getElementById("eight")
+let button9 = document.getElementById("nine")
+let screen = document.getElementById("screen")
+ button1.addEventListener("click", () => {
+  screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg"
+});
 
 
 
-button1.addEventListener("click",()=>{
-  screen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg"
+button2.addEventListener("click", () => {
+  screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg  "
 })
 
-button2.addEventListener("click",()=>{
-  screen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg  "
-})
-
-button3.addEventListener("click",()=>{
-  screen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg  "
-})
-
-
-button4.addEventListener("click",()=>{
-  screen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg"
+button3.addEventListener("click", () => {
+  screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg  "
 })
 
 
-button5.addEventListener("click",()=>{
-  screen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg  "
+button4.addEventListener("click", () => {
+  screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg"
 })
 
 
-button6.addEventListener("click",()=>{
-  screen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg  "
+button5.addEventListener("click", () => {
+  screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg  "
 })
 
 
-button7.addEventListener("click",()=>{
-  screen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg  "
+button6.addEventListener("click", () => {
+  screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg  "
 })
 
 
-button8.addEventListener("click",()=>{
-  screen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg  "
+button7.addEventListener("click", () => {
+  screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg  "
 })
 
 
-button9.addEventListener("click",()=>{
-  screen.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg  "
+button8.addEventListener("click", () => {
+  screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg  "
+})
+
+
+button9.addEventListener("click", () => {
+  screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg  "
 })
 
